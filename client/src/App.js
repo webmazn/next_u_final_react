1275a7ec0f1347884f0fcdf184bdf9af4bc6ac6b
Maycol Zambrano <maycol.zambrano@grupo-zuniga.com>
2020-09-01 00:58:25 -0500

    comunicación con el back-end, capturandor espeusta del servidor

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import './App.css';
+import * as request from 'superagent';
 
 function App() {
   return (
     <div className="App">
-      <div class="container-fluid bg-login">
-        <div class="d-flex justify-content-center h-100">
-          <div class="card">
-            <div class="card-header">
+      <div className="container-fluid bg-login">
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
               <h3>Iniciar Sesión</h3>
-              <span class="resServer"></span>
+              <span className="resServer"></span>
             </div>
-            <div class="card-body">
-              <form>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+            <div className="card-body">
+              <form onSubmit={(e)=> login(e)}>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-user"></i></span>
                   </div>
-                  <input type="email" name="email" class="form-control" placeholder="Correo electrónico"/>
-                  <div class="invalid-feedback">
+                  <input type="email" name="email" id="email" className="form-control" placeholder="Correo electrónico"/>
+                  <div className="invalid-feedback">
                     <div>El email es requerido</div>
                     <div>El email debe ser una dirección válida</div>
                   </div>
                 </div>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-key"></i></span>
                   </div>
-                  <input type="password" name="clave" class="form-control" placeholder="Contraseña"/>
-                  <div class="invalid-feedback">
+                  <input type="password" name="clave" id="clave" className="form-control" placeholder="Contraseña"/>
+                  <div className="invalid-feedback">
                     <div>La clave es requerida</div>
                     <div>La clave debe tener mínimo 6 caracteres</div>
                   </div>
                 </div>
-                <div class="form-group">
-                  <input type="submit" value="Ingresar" class="btn float-right login_btn"/>
+                <div className="form-group">
+                  <input type="submit" value="Ingresar" className="btn float-right login_btn"/>
                 </div>
               </form>
             </div>
@@ -43,6 +44,35 @@ function App() {
       </div>
     </div>
   );
+}
+
+function login(e){
+  e.preventDefault();
+  const API_URI = 'http://localhost:3000';
+
+  request.post(`${API_URI}/login`)
+  .send({
+    email: document.getElementById('email').value,
+    clave: document.getElementById('clave').value
+  })
+  .set('Content-Type','application/json')
+  .end((err, res)=>{
+    // console.log(err);
+    // console.log(res);
+    // console.log(res.body.message);
+    if(err || !res.ok){
+      console.log(res.body.message);
+    }else{
+      console.log("enter");
+    }
+  })
+
+  // .then(resp=>{
+  //   console.log(resp.data.message);
+  // })
+  // .catch(err=>{
+  //   console.log(err);
+  // })
 }
 
 export default App;
