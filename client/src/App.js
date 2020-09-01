cb39982745cd41e49e588687ffb19f5cdb59ed27
Maycol Zambrano <maycol.zambrano@grupo-zuniga.com>
2020-08-31 23:16:55 -0500

    Armando el login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container-fluid bg-login">
+        <div class="d-flex justify-content-center h-100">
+          <div class="card">
+            <div class="card-header">
+              <h3>Iniciar Sesión</h3>
+              <span class="resServer"></span>
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="input-group form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                  </div>
+                  <input type="email" name="email" class="form-control" placeholder="Correo electrónico"/>
+                  <div class="invalid-feedback">
+                    <div>El email es requerido</div>
+                    <div>El email debe ser una dirección válida</div>
+                  </div>
+                </div>
+                <div class="input-group form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                  </div>
+                  <input type="password" name="clave" class="form-control" placeholder="Contraseña"/>
+                  <div class="invalid-feedback">
+                    <div>La clave es requerida</div>
+                    <div>La clave debe tener mínimo 6 caracteres</div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Ingresar" class="btn float-right login_btn"/>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- server/build/controllers/indexController.js
+++ server/build/controllers/indexController.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.indexController = void 0;
 class IndexController {
     index(req, res) {
         res.json({ text: 'My custom API' });
