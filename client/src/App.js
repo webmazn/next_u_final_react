31b1ea5764fba7fc586daf4958e2b22bab7a3114
Maycol Zambrano <maycol.zambrano@grupo-zuniga.com>
2020-09-01 12:23:47 -0500

    Actualizacion de respuesta del servidor

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,132 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import * as request from 'superagent';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container-fluid bg-login">
-        <div className="d-flex justify-content-center h-100">
-          <div className="card">
-            <div className="card-header">
-              <h3>Iniciar Sesión</h3>
-              <span className="resServer"></span>
-            </div>
-            <div className="card-body">
-              <form onSubmit={(e)=> login(e)}>
-                <div className="input-group form-group">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+class App extends Component{
+
+  constructor () {
+    super();
+    this.state = {
+      respuesta: '',
+    };
+  }
+
+  login(e){
+    e.preventDefault();
+    const API_URI = 'http://localhost:3000';
+  
+    request.post(`${API_URI}/login`)
+    .send({
+      email: document.getElementById('email').value,
+      clave: document.getElementById('clave').value
+    })
+    .set('Content-Type','application/json')
+    .end((err, res)=>{
+      // console.log(err);
+      // console.log(res);
+      // console.log(res.body.message);
+      if(err || !res.ok){
+        console.log(res.body.message);
+        this.setState({
+          respuesta: res.body.message
+        });
+      }else{
+        this.setState({
+          respuesta: ''
+        });
+        console.log("enter");
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container-fluid bg-login">
+          <div className="d-flex justify-content-center h-100">
+            <div className="card">
+              <div className="card-header">
+                <h3>Iniciar Sesión</h3>
+                <span className="resServer">{this.state.respuesta}</span>
+              </div>
+              <div className="card-body">
+                <form onSubmit={(e)=> this.login(e)}>
+                  <div className="input-group form-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text"><i className="fas fa-user"></i></span>
+                    </div>
+                    <input type="email" name="email" id="email" className="form-control" placeholder="Correo electrónico"/>
+                    <div className="invalid-feedback">
+                      <div>El email es requerido</div>
+                      <div>El email debe ser una dirección válida</div>
+                    </div>
                   </div>
-                  <input type="email" name="email" id="email" className="form-control" placeholder="Correo electrónico"/>
-                  <div className="invalid-feedback">
-                    <div>El email es requerido</div>
-                    <div>El email debe ser una dirección válida</div>
+                  <div className="input-group form-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text"><i className="fas fa-key"></i></span>
+                    </div>
+                    <input type="password" name="clave" id="clave" className="form-control" placeholder="Contraseña"/>
+                    <div className="invalid-feedback">
+                      <div>La clave es requerida</div>
+                      <div>La clave debe tener mínimo 6 caracteres</div>
+                    </div>
                   </div>
-                </div>
-                <div className="input-group form-group">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                  <div className="form-group">
+                    <input type="submit" value="Ingresar" className="btn float-right login_btn"/>
                   </div>
-                  <input type="password" name="clave" id="clave" className="form-control" placeholder="Contraseña"/>
-                  <div className="invalid-feedback">
-                    <div>La clave es requerida</div>
-                    <div>La clave debe tener mínimo 6 caracteres</div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <input type="submit" value="Ingresar" className="btn float-right login_btn"/>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
-function login(e){
-  e.preventDefault();
-  const API_URI = 'http://localhost:3000';
-
-  request.post(`${API_URI}/login`)
-  .send({
-    email: document.getElementById('email').value,
-    clave: document.getElementById('clave').value
-  })
-  .set('Content-Type','application/json')
-  .end((err, res)=>{
-    // console.log(err);
-    // console.log(res);
-    // console.log(res.body.message);
-    if(err || !res.ok){
-      console.log(res.body.message);
-    }else{
-      console.log("enter");
-    }
-  })
-
-  // .then(resp=>{
-  //   console.log(resp.data.message);
-  // })
-  // .catch(err=>{
-  //   console.log(err);
-  // })
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="container-fluid bg-login">
+//         <div className="d-flex justify-content-center h-100">
+//           <div className="card">
+//             <div className="card-header">
+//               <h3>Iniciar Sesión</h3>
+//               <span className="resServer"></span>
+//             </div>
+//             <div className="card-body">
+//               <form onSubmit={(e)=> login(e)}>
+//                 <div className="input-group form-group">
+//                   <div className="input-group-prepend">
+//                     <span className="input-group-text"><i className="fas fa-user"></i></span>
+//                   </div>
+//                   <input type="email" name="email" id="email" className="form-control" placeholder="Correo electrónico"/>
+//                   <div className="invalid-feedback">
+//                     <div>El email es requerido</div>
+//                     <div>El email debe ser una dirección válida</div>
+//                   </div>
+//                 </div>
+//                 <div className="input-group form-group">
+//                   <div className="input-group-prepend">
+//                     <span className="input-group-text"><i className="fas fa-key"></i></span>
+//                   </div>
+//                   <input type="password" name="clave" id="clave" className="form-control" placeholder="Contraseña"/>
+//                   <div className="invalid-feedback">
+//                     <div>La clave es requerida</div>
+//                     <div>La clave debe tener mínimo 6 caracteres</div>
+//                   </div>
+//                 </div>
+//                 <div className="form-group">
+//                   <input type="submit" value="Ingresar" className="btn float-right login_btn"/>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
